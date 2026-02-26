502869ed0cb353e6ba7202cc04ff8f41c000d125
PIN0L33KZ <76883164+PIN0L33KZ@users.noreply.github.com>
2026-02-26 16:22:02 +0100

    Fix user loading on files, console and profile pages

--- userProfile.php
+++ userProfile.php
@@ -1,10 +1,14 @@
 <?php
 require_once 'inc/lib.php';
 session_start();
-if (empty($_SESSION['user']) || !user_info($_SESSION['user'])) {
+
+$user = !empty($_SESSION['user']) ? user_info($_SESSION['user']) : false;
+if (!$user) {
 	header('Location: dashboard.php');
 	exit('Not Authorized');
 }
+
+$_SESSION['is_admin'] = $user['role'] === 'admin';
 ?>
 <!doctype html>
 <html lang="en">
